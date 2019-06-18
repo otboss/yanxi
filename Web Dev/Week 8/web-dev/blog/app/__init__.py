f231c4a1df2b8463f9d79ced6ebbbf2f4736c1b2
Villa <d.thomas71@yahoo.com>
2019-06-18 22:21:09 +0800

    added week 10, 9, 8 for web dev

---
+++ Web Dev/Week 8/web-dev/blog/app/__init__.py
@@ -0,0 +1,19 @@
+#!../../flask/bin/python
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_mail import Mail
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+mail = Mail(app)
+
+from app import models, routes
+
+login.login_view = 'login'
